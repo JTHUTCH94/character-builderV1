0ebe39e149360eeaf4de114a5d530f5e8997a5a0
Joshua Hutchinson <jthutch94@gmail.com>
2018-04-17 10:34:05 -0400

    added enter-page

--- src/CharacterForm.js
+++ src/CharacterForm.js
@@ -6,7 +6,39 @@ import Torch1 from './components/Torch1';
 import Torch2 from './components/Torch2';
 
 class CharacterForm extends Component {
+  constructor(){
+  super();
+
+  this.state={
+    display: 'hidden'
+  }
+  };
+
+  showForm(){
+    this.setState({
+      display: 'form'
+    });
+  }
+
   render() {
+    if(this.state.display === 'hidden'){
+      return (
+      <div className="enter-page">
+      <Torch1 />
+      <h1 className="enter-sign">Follow the Flame to Begin</h1>
+      <input type="button" value="Enter" onClick={e => this.showForm()} className="enter"/>
+      <div className="torch3">
+        <div className="flame3">
+            <div className="red flame"></div>
+            <div className="orange flame"></div>
+            <div className="yellow flame"></div>
+            <div className="white flame"></div>
+        </div>
+        </div>
+        <label className="enter-label" />
+      <Torch2 />
+      </div>)
+    } else if(this.state.display === 'form'){
     return (
       <div className="form-body">
       <Torch1 />
@@ -18,6 +50,7 @@ class CharacterForm extends Component {
       </div>
     );
   }
+}
 }
 
 export default CharacterForm;
