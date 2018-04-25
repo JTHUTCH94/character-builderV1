938ce3d49daa238f9d0c2d12d94f5fdf502ea6db
Joshua Hutchinson <jthutch94@gmail.com>
2018-04-25 17:26:36 -0400

    refactored for redux

--- character-builderV1-client/src/actions.js
+++ character-builderV1-client/src/actions.js
@@ -4,17 +4,102 @@ export const createCharacter = (newCharacter) => ({
     newCharacter
 });
 
-export const GET_CHARACTER = 'GET_CHARACTER';
-export const getCharacter = (character) => ({
-    type: GET_CHARACTER,
-    character
+export const createCharacterAction = (newCharacter) => dispatch => {
+    return fetch('http://localhost:8080/api/characters', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            "name": this.newCharacter.name,
+            "race": this.newCharacter.race,
+            "class": this.newCharacter.class,
+            "weapon": this.newCharacter.weapon
+        })
+        .then(res => res.json())
+        .then(res => dispatch(createCharacter(res)))
+    });
+};
+
+export const SET_NAME = 'SET_NAME';
+export const setName = (e) => ({
+    type: SET_NAME,
+    e
 });
 
-export const DELETE_CHARACTER = 'DELETE_CHARACTER';
-export const deleteCharacter = (character) => ({
-    type: DELETE_CHARACTER,
-    character
+export const setNameAction = (e) => {
+    this.props.dispatch(setName(e.target.value));
+}
+
+export const SET_RACE = 'SET_RACE';
+export const setRace = (e) => ({
+    type: SET_RACE,
+    e
 });
+
+export const setRaceAction = (e) => {
+    this.props.dispatch(setRace(e.target.value));
+};
+
+export const SET_CLASS = 'SET_CLASS';
+export const setClass = (e) => ({
+    type: SET_CLASS,
+    e
+});
+
+export const setClassAction = (e) => {
+    this.props.dispatch(setClass(e.target.value));
+};
+
+export const SET_WEAPON = 'SET_WEAPON';
+export const setWeapon = (e) => ({
+    type: SET_WEAPON,
+    e
+});
+
+export const setWeaponAction = (e) => {
+    this.props.dispatch(setWeapon(e.target.value));
+}
+
+export const SET_DISPLAY = 'SET_DISPLAY';
+export const setDisplay = (e) => ({
+    type: SET_DISPLAY,
+    e
+});
+
+export const setDisplayAction = (e) => {
+    this.setState({
+        display: 'new-character'
+    });
+}
+
+export const GET_CHARACTERS = 'GET_CHARACTERS';
+export const getCharacters = (characters) => ({
+    type: GET_CHARACTERS,
+    characters
+});
+
+
+export const getCharactersAction = (characters) => dispatch => {
+    return fetch('http://localhost:8080/api/characters', {
+        headers: { 'Content-Type': 'application/json' }
+    })
+        .then(res => res.json())
+        .then(res => dispatch(getCharacters(res)));
+}
+
+export const DELETE_CHARACTER = 'DELETE_CHARACTER';
+export const deleteCharacter = (id) => ({
+    type: DELETE_CHARACTER,
+    id
+});
+
+export const deleteCharacterAction = (id) => dispatch =>{
+
+    return fetch('http://localhost:8080/api/characters/' + id, {
+        method: 'DELETE'
+    })
+    .then(res => dispatch(deleteCharacter(id)))
+    .catch(err => console.log(err));
+}
 
 /*export const UPDATE_CHARACTER = 'UPDATE_CHARACTER';
 export const updateCharacter = (character) => ({
--- character-builderV1-client/src/reducer.js
+++ character-builderV1-client/src/reducer.js
@@ -1,7 +1,8 @@
-import { CREATE_CHARACTER, GET_CHARACTER, DELETE_CHARACTER } from './actions';
+import { CREATE_CHARACTER, GET_CHARACTERS, DELETE_CHARACTER, SET_NAME, SET_RACE, SET_CLASS, SET_WEAPON, SET_DISPLAY } from './actions';
 
 const initialState = {
     characters: [],
+    display: 'form',
     name: '',
     race: '',
     class: '',
@@ -11,18 +12,39 @@ const initialState = {
 export default (state = initialState, action) => {
     if(action.type === CREATE_CHARACTER) {
         return Object.assign({}, state, {
-            name: action.newCharacter.name,
-            race: action.newCharacter.race,
-            class: action.newCharacter.class,
-            weapon: action.newCharacter.weapon
+            name: action.name,
+            race: action.race,
+            class: action.class,
+            weapon: action.weapon
         });
-    } else if (action.type === GET_CHARACTER){
+    } else if(action.type === SET_NAME){
         return Object.assign({}, state, {
-            characters: action.character
+            name: action.name
+        });
+    } else if(action.type === SET_RACE){
+        return Object.assign({}, state, {
+            race: action.race
+        });
+    } else if(action.type === SET_CLASS){
+        return Object.assign({}, state, {
+            class: action.class
+        });
+    } else if(action.type === SET_WEAPON){
+        return Object.assign({}, state, {
+            weapon: action.weapon
+        });
+    } else if(action.type === SET_DISPLAY){
+        return Object.assign({}, state, {
+            display: 'newCharacter'
+        });
+    } else if (action.type === GET_CHARACTERS){
+        return Object.assign({}, state, {
+            characters: action.characters,
+            display: 'characters'
         });
     } else if(action.type === DELETE_CHARACTER){
         return Object.assign({}, state, {
-            characters: state.characters
+            characters: state.characters.filter(character => !character.id === action.id)
         });
     }
     return state;
